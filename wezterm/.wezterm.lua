59089e85ca212ad1e5fd0be76721a575a5e1e5bf
Theo Keane <20931519+theochemtheo@users.noreply.github.com>
2025-01-03 09:50:40 +0000

    wezterm font + formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,25 +3,24 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 local mux = wezterm.mux
 
-config.font = wezterm.font('MesloLGS NF')
+config.font = wezterm.font('MesloLGS NF',
+                           {weight = "Regular", stretch = "Condensed"})
+config.font_size = 10.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
 config.keys = {
-    { key = 'LeftArrow', mods = 'ALT|CMD', action = act.ActivateTabRelative(-1)},
-    { key = 'RightArrow', mods = 'ALT|CMD', action = act.ActivateTabRelative(1)},
+    {key = 'LeftArrow', mods = 'ALT|CMD', action = act.ActivateTabRelative(-1)},
+    {key = 'RightArrow', mods = 'ALT|CMD', action = act.ActivateTabRelative(1)}
 }
 
 config.window_decorations = 'RESIZE'
-config.window_frame = {
-    font = wezterm.font('MesloLGS NF'),
-    font_size = 11,
-}
+config.window_frame = {font = wezterm.font('MesloLGS NF'), font_size = 10}
 
 wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
 end)
 
 config.window_close_confirmation = 'NeverPrompt'
